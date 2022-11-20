7fbca80a6b7fb95e169ac4d1933ae2028c5a103f
KamilMammadov <kamiltm@code.edu.az>
2022-11-20 18:18:17 +0400

    FinCode validation written

---
+++ DemoWorkerManagement/Utility/FinCodeValidation.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace DemoWorkerManagement.Utility
+{
+    public class FinCodeValidation : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+           Regex regex = new Regex(@"^[A-Z0-9]{7}$");
+
+            if (value != null)
+            {
+                string FinCode = value.ToString();
+                if (regex.IsMatch(FinCode))
+                {
+                    return ValidationResult.Success;
+                }
+            }
+            return new ValidationResult("Error");
+        }
+    }
+}
