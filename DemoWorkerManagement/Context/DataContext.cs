c9e34bae1e70384a1aec90d1e7022856c1765f37
KamilMammadov <kamiltm@code.edu.az>
2022-11-20 18:17:00 +0400

    Database configured

--- DemoWorkerManagement/Context/DataContext.cs
+++ DemoWorkerManagement/Context/DataContext.cs
@@ -1,14 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
-using WorkerManagement.Models;
+﻿using DemoWorkerManagement.Models;
+using Microsoft.EntityFrameworkCore;
 
-namespace WorkerManagement.Context
+namespace DemoWorkerManagement.Context
 {
     public class DataContext : DbContext
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=DESKTOP-466PC0V;Database=Workers;Trusted_Connection=True;TrustServerCertificate=True;"); 
+            optionsBuilder.UseSqlServer("Server=DESKTOP-466PC0V;Database=Workers;Trusted_Connection=True;TrustServerCertificate=True;");
         }
         public DbSet<Worker> Workers { get; set; }
+
+
     }
 }
---
+++ DemoWorkerManagement/Context/WorkerPkCode.cs
@@ -0,0 +1,45 @@
+﻿namespace DemoWorkerManagement.Context
+{
+    public class WorkerPkCode
+    {
+
+        static Random _random = new Random();
+        private static string _workerCode;
+
+        public static string WorkerCode
+        {
+            get
+            {
+                DataContext dataContext = new DataContext();
+                var workers = dataContext.Workers.ToList();
+
+                bool status = true;
+                string newCode = "E" + _random.Next(10000, 100000);
+
+                while (status)
+                {
+                    int lastCode = 0;
+
+                    foreach (var worker in workers)
+                    {
+                        if (worker.WorkerCode == newCode)
+                        {
+                            do
+                            {
+                                newCode = "E" + _random.Next(10000, 100000);
+
+                            } while (worker.WorkerCode != newCode);
+                        }
+                        lastCode++;
+
+                    }
+                    if (lastCode == workers.Count)
+                    {
+                        status = false;
+                    }
+                }
+                return newCode;
+            }
+        }
+    }
+}
